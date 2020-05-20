053d18d0bec17b6eac4746fb1268ba5a269df046
Seth <thatguythatsbadwithnames@gmail.com>
2020-05-20 15:42:56 -0400

    extract data working with names and phone numbers

--- xlsScraper.go
+++ xlsScraper.go
@@ -1,3 +1,60 @@
 package scraper
 
-//remember tdd
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/360EntSecGroup-Skylar/excelize"
+)
+
+//extractData takes a .xlsx filename (including the ".xlsx") as a string
+//and returns all of the data in columns B and J.
+//TODO: make sheet parameter
+//TODO: make it get more info: create a struct of personinfo and store phone,
+//spirit survey, soc events, bstud, and follow up status
+func extractData(filename string) (map[string]string, error) {
+	//open file, check for error
+	file, err := excelize.OpenFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	//create map
+	people := make(map[string]string)
+
+	i := 0
+
+	//loop through all cells and build string with names and phone numbers
+	//until empty name cell found
+	for {
+
+		//needed b/c input starts at second
+		//cell in sheet.
+		iConv := strconv.Itoa((i + 2))
+
+		//get name value
+		cellName, err := file.GetCellValue("Men", "B"+iConv)
+		if err != nil {
+			fmt.Println(err)
+			return people, err
+		}
+		//check to make sure an entry exists, if not, stop loop
+		if cellName == "" {
+			break
+		}
+
+		//get phone number value
+		cellNum, err := file.GetCellValue("Men", "J"+iConv)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		people[cellName] = cellNum
+		i++
+	}
+
+	//if no error, return value
+	return people, nil
+}
--- xlsScrapter_test.go
+++ xlsScrapter_test.go
@@ -1,9 +1,8 @@
 package scraper
 
 import (
+	"reflect"
 	"testing"
-
-	"github.com/360EntSecGroup-Skylar/excelize"
 )
 
 //first step is test a fucntion to open file and print first cell: A1
@@ -13,12 +12,48 @@ import (
 //test opening file
 func TestCLI(t *testing.T) {
 
-	t.Run("Open local .xlsx file with reader", func(t *testing.T) {
-		got, err := excelize.OpenFile("Barringer.xlsx")
-		want := *excelize.File{}
+	t.Run("extractData returns first all names with their phone numbers", func(t *testing.T) {
+		got, err := extractData("Barringer.xlsx")
+		want := map[string]string{
+			"seth p":  "(201) 742-6796",
+			"chris c": "(984) 938-7156",
+			"fiona r": "(354) 388-5157",
+			"paul o":  "(893) 828-6784",
+			"meg s":   "(281) 658-2936",
+			"baily v": "(841) 508-0541",
+			"test1":   "(428) 965-2160",
+			"test2":   "(575) 770-5630",
+			"test3":   "(947) 472-4409",
+			"test4":   "(617) 670-6033",
+			"test5":   "(724) 992-5937",
+			"test6":   "(385) 494-5408",
+			"test7":   "(646) 677-9739",
+			"test8":   "(263) 262-5372",
+			"test9":   "(610) 213-4025",
+			"test10":  "(946) 623-8615",
+			"test11":  "(499) 881-3999",
+			"test12":  "(963) 315-7080",
+			"test13":  "(996) 656-4599",
+			"test14":  "(721) 271-5887",
+			"test15":  "(516) 458-8837",
+			"test16":  "(600) 687-5571",
+			"test17":  "(688) 992-4106",
+			"test18":  "(290) 745-1183",
+			"test19":  "(510) 224-7039",
+			"test20":  "(278) 580-9114",
+			"test21":  "(509) 991-5196",
+			"test22":  "(810) 572-7101",
+			"test23":  "(783) 773-6362",
+			"test24":  "(687) 350-8556",
+			"test25":  "(504) 542-5459",
+			"test26":  "(643) 817-2536"}
 
-		if got != want {
-			t.Errorf("got %v want %v", got, want)
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %s want %s", got, want)
+		}
+
+		if err != nil {
+			t.Errorf("Did not expect an error but got %s", err)
 		}
 	})
 }
