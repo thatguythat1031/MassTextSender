80c3a9e9a29cf45ac7d7124305b3fcbc27ee4dc8
Seth <thatguythatsbadwithnames@gmail.com>
2020-05-21 19:24:41 -0400

    changed ExtractData map to use PersonInfo struct, changed testing

--- xlsScraper.go
+++ xlsScraper.go
@@ -3,16 +3,26 @@ package scraper
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
 
-//extractData takes a .xlsx filename (including the ".xlsx") as a string
+//PersonInfo holds all of the information that needs to be extracted from the
+//.xlsx file. Follow up status should be "Contacted", "Uncontacted", or "Attempting Contact"
+type PersonInfo struct {
+	phone        string
+	followUpStat string
+	spiritSurv   bool
+	socEvent     bool
+	bStud        bool
+}
+
+//ExtractData takes a .xlsx filename (including the ".xlsx") as a string
 //and returns all of the data in columns B and J.
-//TODO: make sheet parameter
 //TODO: make it get more info: create a struct of personinfo and store phone,
 //spirit survey, soc events, bstud, and follow up status
-func extractData(filename string) (map[string]string, error) {
+func ExtractData(filename string, sheetname string) (map[string]PersonInfo, error) {
 	//open file, check for error
 	file, err := excelize.OpenFile(filename)
 	if err != nil {
@@ -21,7 +31,7 @@ func extractData(filename string) (map[string]string, error) {
 	}
 
 	//create map
-	people := make(map[string]string)
+	people := make(map[string]PersonInfo)
 
 	i := 0
 
@@ -34,7 +44,7 @@ func extractData(filename string) (map[string]string, error) {
 		iConv := strconv.Itoa((i + 2))
 
 		//get name value
-		cellName, err := file.GetCellValue("Men", "B"+iConv)
+		cellName, err := file.GetCellValue(sheetname, "B"+iConv)
 		if err != nil {
 			fmt.Println(err)
 			return people, err
@@ -45,16 +55,67 @@ func extractData(filename string) (map[string]string, error) {
 		}
 
 		//get phone number value
-		cellNum, err := file.GetCellValue("Men", "J"+iConv)
+		cellNumPhone, err := file.GetCellValue(sheetname, "J"+iConv)
 		if err != nil {
 			fmt.Println(err)
 			return nil, err
 		}
 
-		people[cellName] = cellNum
+		//get followUpStat value
+		cellFollowUp, err := file.GetCellValue(sheetname, "P"+iConv)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		//get spiritSurv value
+		cellSpiritSurv, err := file.GetCellValue(sheetname, "L"+iConv)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		spiritSurvVal := convertCellStringToBool(cellSpiritSurv)
+
+		//get socEvent value
+		cellSocEvent, err := file.GetCellValue(sheetname, "L"+iConv)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		socEventVal := convertCellStringToBool(cellSocEvent)
+
+		//get bstud value
+		cellBStud, err := file.GetCellValue(sheetname, "L"+iConv)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		bStudVal := convertCellStringToBool(cellBStud)
+
+		people[cellName] = PersonInfo{
+			phone:        cellNumPhone,
+			followUpStat: cellFollowUp,
+			spiritSurv:   spiritSurvVal,
+			socEvent:     socEventVal,
+			bStud:        bStudVal,
+		}
 		i++
 	}
 
-	//if no error, return value
+	//if no error, return map
 	return people, nil
+}
+
+//Takes the given cell value and converts it to a bool for use in
+//the PersonInfo struct
+func convertCellStringToBool(cellString string) bool {
+	var convertedBool bool
+
+	if cellString == "" || strings.EqualFold("no", cellString) {
+		convertedBool = false
+	} else { //if cell is "maybe" or "yes"
+		convertedBool = true
+	}
+
+	return convertedBool
 }
--- xlsScraper_test.go
+++ xlsScraper_test.go
@@ -5,55 +5,55 @@ import (
 	"testing"
 )
 
-//first step is test a fucntion to open file and print first cell: A1
-//also will need to navigate to "MEN" sheet so that may need to be an option later
-//looks like will likely be using excelize library
-
-//test opening file
 func TestCLI(t *testing.T) {
 
-	t.Run("extractData returns first all names with their phone numbers", func(t *testing.T) {
-		got, err := extractData("Barringer.xlsx")
-		want := map[string]string{
-			"seth p":  "(201) 742-6796",
-			"chris c": "(984) 938-7156",
-			"fiona r": "(354) 388-5157",
-			"paul o":  "(893) 828-6784",
-			"meg s":   "(281) 658-2936",
-			"baily v": "(841) 508-0541",
-			"test1":   "(428) 965-2160",
-			"test2":   "(575) 770-5630",
-			"test3":   "(947) 472-4409",
-			"test4":   "(617) 670-6033",
-			"test5":   "(724) 992-5937",
-			"test6":   "(385) 494-5408",
-			"test7":   "(646) 677-9739",
-			"test8":   "(263) 262-5372",
-			"test9":   "(610) 213-4025",
-			"test10":  "(946) 623-8615",
-			"test11":  "(499) 881-3999",
-			"test12":  "(963) 315-7080",
-			"test13":  "(996) 656-4599",
-			"test14":  "(721) 271-5887",
-			"test15":  "(516) 458-8837",
-			"test16":  "(600) 687-5571",
-			"test17":  "(688) 992-4106",
-			"test18":  "(290) 745-1183",
-			"test19":  "(510) 224-7039",
-			"test20":  "(278) 580-9114",
-			"test21":  "(509) 991-5196",
-			"test22":  "(810) 572-7101",
-			"test23":  "(783) 773-6362",
-			"test24":  "(687) 350-8556",
-			"test25":  "(504) 542-5459",
-			"test26":  "(643) 817-2536"}
+	t.Run("extractData returns all names with their PersonInfo as a map", func(t *testing.T) {
+		got, err := ExtractData("Barringer.xlsx", "Men")
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %s want %s", got, want)
+		//testing 3 arbitrary entries from map and the length instead of every entry to make
+		//testing less tedious
+		want1 := PersonInfo{
+			phone:        "(201) 742-6796",
+			followUpStat: "Contacted",
+			spiritSurv:   false,
+			socEvent:     false,
+			bStud:        false,
+		}
+		want2 := PersonInfo{
+			phone:        "(428) 965-2160",
+			followUpStat: "Attempting Contact",
+			spiritSurv:   true,
+			socEvent:     true,
+			bStud:        true,
+		}
+		want3 := PersonInfo{
+			phone:        "(278) 580-9114",
+			followUpStat: "Uncontacted",
+			spiritSurv:   false,
+			socEvent:     false,
+			bStud:        false,
+		}
+
+		if !reflect.DeepEqual(got["seth p"], want1) {
+			t.Errorf("got %v want %v", got["seth p"], want1)
+		}
+		if !reflect.DeepEqual(got["test1"], want2) {
+			t.Errorf("got %v want %v", got["test1"], want2)
+		}
+		if !reflect.DeepEqual(got["test20"], want3) {
+			t.Errorf("got %v want %v", got["test20"], want3)
 		}
 
 		if err != nil {
 			t.Errorf("Did not expect an error but got %s", err)
+		}
+	})
+
+	t.Run("attempt to extract from non-present file", func(t *testing.T) {
+		_, err := ExtractData("Miles.xlsx", "All")
+
+		if err == nil {
+			t.Errorf("Expected an error but did not get one")
 		}
 	})
 }
